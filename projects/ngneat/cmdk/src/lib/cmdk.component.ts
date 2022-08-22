22f6fbb29ffa7ce996daa2b1b488873405f0988c
Dharmen Shah <shhdharmen@gmail.com>
2022-08-22 18:12:40 +0530

    chore: add angular-eslint

--- projects/ngneat/cmdk/src/lib/cmdk.component.ts
+++ projects/ngneat/cmdk/src/lib/cmdk.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-cmdk',
-  template: `
-    <p>
-      cmdk works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>cmdk works!</p> `,
+  styles: [],
 })
-export class CmdkComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CmdkComponent {
+  constructor() {}
 }
