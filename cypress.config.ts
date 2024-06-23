fff473ec9310909967d2449d099e45a7476e4544
Dharmen Shah <shhdharmen@gmail.com>
2024-06-23 08:28:30 +0000

    ci: changes for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
-const { defineConfig } = require('cypress');
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   component: {
     devServer: {
       framework: 'angular',
@@ -8,4 +8,4 @@ module.exports = defineConfig({
     },
     specPattern: '**/*.cy.ts',
   },
-});
+})
