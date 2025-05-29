5b0470ddf2219e30cd9c988e9d3cdfdf34eeb1f7
Dharmen Shah <shhdharmen@gmail.com>
2025-05-29 09:29:57 +0000

    chore: fix cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
-      options: {
-        projectConfig: {
-          root: './',
-          sourceRoot: 'src',
-          buildOptions: {
-            outputPath: 'dist/browser',
-          },
-        },
-      },
     },
     specPattern: '**/*.cy.ts',
   },
-});
+})
